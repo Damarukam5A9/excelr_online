e7c67b63118de2561bbd36722b2b0dbca3f45ee0
unknown <damarnath8919@gmail.com>
2025-04-24 17:37:57 +0530

    day24

---
+++ day 23/dom1.js
@@ -0,0 +1,39 @@
+// accessing element with getElementbyId()
+
+let heading=document.getElementById('heading');
+heading.style.color='red';
+console.log(heading);
+
+
+// accessing element with getElementsByClassName()
+let heading2s=document.getElementsByClassName('heading2');
+for(let e of heading2){
+    e.style.color='green';
+}
+console.log(heading2);
+
+// accessing element with getElementsByTagName()
+let heading3=document.getElementsByTagName('h3');
+for(let e of heading3){
+    e.style.color='blue';
+}
+
+//accessing element with querySelector()
+let p=document.querySelector('.p1');
+p.style.color='red';
+//accessing element with querySelectorAll()
+let pLists=document.querySelectorAll('.p1');
+console.log(pLists);
+
+for(let e of pLists){
+    e.style.fontsize='40px';
+}
+
+// creation of element with createElement()
+
+let pCreate=document.createElement('p');
+pCreate.textcontent='this is a new paragraph created by using textContent property';
+
+let container=document.querySelector('.container');
+container.appendChild(pcreate);
+
